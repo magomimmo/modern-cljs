0d1679c4b5c251651eb9b1e82335fbd9bb22cddf
magomimmo <mimmo.cosenza@gmail.com>
2012-11-07 01:02:55 +0100

    first tutorial 4 release

---
+++ src/cljs/modern_cljs/login.cljs
@@ -0,0 +1,24 @@
+(ns modern-cljs.login)
+
+(defn validate-form []
+  ;; get email and password element by input ids
+  (let [email (.getElementById js/document "email")
+        password (.getElementById js/document "password")]
+    (if (and (> (count (.-value email)) 0)
+             (> (count (.-value password)) 0))
+      true
+      (do (js/alert "Please, complete the form!")
+          false))))
+
+(defn init []
+  ;; verity that js/document exists and that it has a getElementById
+  ;; property
+  (if (and js/document
+           (.-getElementById js/document))
+    ;; get loginForm by element id and set its onsubmit property to
+    ;; validate-form function
+    (let [login-form (.getElementById js/document "loginForm")]
+      (set! (.-onsubmit login-form) validate-form))))
+
+;; when js/window has been loaded, set its onload property to init function
+(set! (.-onload js/window) init)
