b7a01b352bdec73c68525416e128b0e9c8003d66
magomimmo <mimmo.cosenza@gmail.com>
2012-12-09 20:37:27 +0100

    added :min-lein-version option to warn any user using lein 1.7

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   ; clojure source code path
   :source-paths ["src/clj"
                  "compiler/clojurescript/src/clj"
