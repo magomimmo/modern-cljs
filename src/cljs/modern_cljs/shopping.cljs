59db6d8ac718f0d512b3967b4ae9bda0a4eefe3c
Giacomo Cosenza <giacomo.cosenza@sinapsi.com>
2013-01-15 20:08:11 +0100

    updated :requir :as to :require :refer and substituted by-class with js interop

--- src/cljs/modern_cljs/shopping.cljs
+++ src/cljs/modern_cljs/shopping.cljs
@@ -1,29 +1,30 @@
 (ns modern-cljs.shopping
-  (:require-macros [hiccups.core :as h])
-  (:require [domina :as dom]
-            [domina.events :as ev]
+  (:require-macros [hiccups.core :refer [html]])
+  (:require [domina :refer [by-id by-class value set-value! append! destroy!]]
+            [domina.events :refer [listen!]]
             [shoreleave.remotes.http-rpc :refer [remote-callback]]
             [cljs.reader :refer [read-string]]))
 
 (defn calculate []
-  (let [quantity (read-string (dom/value (dom/by-id "quantity")))
-        price (read-string (dom/value (dom/by-id "price")))
-        tax (read-string (dom/value (dom/by-id "tax")))
-        discount (read-string (dom/value (dom/by-id "discount")))]
+  (let [quantity (read-string (value (by-id "quantity")))
+        price (read-string (value (by-id "price")))
+        tax (read-string (value (by-id "tax")))
+        discount (read-string (value (by-id "discount")))]
     (remote-callback :calculate
                      [quantity price tax discount]
-                     #(dom/set-value! (dom/by-id "total") (.toFixed % 2)))))
+                     #(set-value! (by-id "total") (.toFixed % 2)))))
 
 (defn add-help []
-  (dom/append! (dom/by-id "shoppingForm")
-               (h/html [:div.help "Click to calculate"])))
+  (append! (by-id "shoppingForm")
+               (html [:div.help "Click to calculate"])))
 
 (defn remove-help []
-  (dom/destroy! (dom/by-class "help")))
-
+  ;;(destroy! (by-class "help")))
+  (destroy! (.getElementsByClassName js/document "help")))
+  
 (defn ^:export init []
   (when (and js/document
              (aget js/document "getElementById"))
-    (ev/listen! (dom/by-id "calc") :click calculate)
-    (ev/listen! (dom/by-id "calc") :mouseover add-help)
-    (ev/listen! (dom/by-id "calc") :mouseout remove-help)))
+    (listen! (by-id "calc") :click calculate)
+    (listen! (by-id "calc") :mouseover add-help)
+    (listen! (by-id "calc") :mouseout remove-help)))
