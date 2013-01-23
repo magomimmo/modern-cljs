3f8ccfb38eae4fe558b59f1f60eb9df5f0e04df7
Giacomo Cosenza <giacomo.cosenza@sinapsi.com>
2013-01-23 20:24:59 +0100

    added POST route

--- src/clj/modern_cljs/core.clj
+++ src/clj/modern_cljs/core.clj
@@ -1,20 +1,22 @@
 (ns modern-cljs.core
-  (:require [compojure.core :refer [defroutes GET]]
+  (:require [compojure.core :refer [defroutes GET POST]]
             [compojure.route :refer [resources not-found]]
-            [compojure.handler :refer [site]]))
+            [compojure.handler :refer [site]]
+            [modern-cljs.login :refer [validate-user]]))
 
 ;; defroutes macro defines a function that chains individual route
 ;; functions together. The request map is passed to each function in
 ;; turn, until a non-nil response is returned.
 (defroutes app-routes
-  ; to serve document root address
+  ;; to serve document root address
   (GET "/" [] "<p>Hello from compojure</p>")
-  ; to server static pages saved in resources/public directory
+  ;; to server static pages saved in resources/public directory
+  (POST "/login" [email password] (validate-user email password)) 
   (resources "/")
-  ; if page is not found
+  ;; if page is not found
   (not-found "Page non found"))
 
-;; site function create an handler suitable for a standard website,
-;; adding a bunch of standard ring middleware to app-route:
+;;; site function create an handler suitable for a standard website,
+;;; adding a bunch of standard ring middleware to app-route:
 (def handler
   (site app-routes))
