2609ed9b9de9653b73dcfc220da6841b0ecb85cb
Giacomo Cosenza <giacomo.cosenza@sinapsi.com>
2013-03-04 16:05:53 +0100

    lein-cljsbuild updated to 0.3.0 and lein-ring updated to 0.8.3

--- project.clj
+++ project.clj
@@ -15,8 +15,8 @@
                  [com.cemerick/shoreleave-remote-ring "0.0.2"]
                  [shoreleave/shoreleave-remote "0.2.2"]]
 
-  :plugins [[lein-cljsbuild "0.2.9"]
-            [lein-ring "0.8.0-SNAPSHOT"]]
+  :plugins [[lein-cljsbuild "0.3.0"]
+            [lein-ring "0.8.3"]]
 
   ;; enable cljsbuild tasks support
   ;; :hooks [leiningen.cljsbuild]
