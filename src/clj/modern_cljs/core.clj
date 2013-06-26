a15dcdab3165944b232019a52a47137052faa49d
Giacomo Cosenza <giacomo.cosenza@sinapsi.com>
2013-06-26 17:59:31 +0200

    add shopping route and refactor the code to solve cyclic namespace dependency

--- src/clj/modern_cljs/core.clj
+++ src/clj/modern_cljs/core.clj
@@ -2,7 +2,9 @@
   (:require [compojure.core :refer [defroutes GET POST]]
             [compojure.route :refer [resources not-found]]
             [compojure.handler :refer [site]]
-            [modern-cljs.login :refer [authenticate-user]]))
+            [modern-cljs.login :refer [authenticate-user]]
+            [modern-cljs.templates.shopping :refer [shopping]]
+            [shoreleave.middleware.rpc :refer [wrap-rpc]]))
 
 ;; defroutes macro defines a function that chains individual route
 ;; functions together. The request map is passed to each function in
@@ -12,6 +14,9 @@
   (GET "/" [] "<p>Hello from compojure</p>")
   ;; to authenticate the user
   (POST "/login" [email password] (authenticate-user email password))
+  ;; to server shopping command
+  (POST "/shopping" [quantity price tax discount]
+        (shopping quantity price tax discount))
   ;; to server static pages saved in resources/public directory
   (resources "/")
   ;; if page is not found
@@ -21,3 +26,8 @@
 ;;; adding a bunch of standard ring middleware to app-route:
 (def handler
   (site app-routes))
+
+(def app (-> (var handler)
+             (wrap-rpc)
+             (site)))
+
