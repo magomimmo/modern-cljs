ffa2cce0b3f814f273dacd518ce4c641c1f868c7
Giacomo Cosenza <giacomo.cosenza@sinapsi.com>
2013-06-26 18:03:12 +0200

    updated namespace declaration, calculate redefined and app removed

--- src/clj/modern_cljs/remotes.clj
+++ src/clj/modern_cljs/remotes.clj
@@ -1,17 +1,17 @@
 (ns modern-cljs.remotes
-  (:require [modern-cljs.core :refer [handler]]
-            [modern-cljs.login.java.validators :as v]
+  (:require [modern-cljs.login.java.validators :as v]
             [compojure.handler :refer [site]]
-            [shoreleave.middleware.rpc :refer [defremote wrap-rpc]]))
+            [modern-cljs.utils :refer [parse-integer parse-double]]
+            [shoreleave.middleware.rpc :refer [defremote]]))
 
 (defremote calculate [quantity price tax discount]
-  (-> (* quantity price)
-      (* (+ 1 (/ tax 100)))
-      (- discount)))
+  (let [q (parse-integer quantity)
+        p (parse-double price)
+        t (parse-double tax)
+        d (parse-double discount)]
+  (-> (* q p)
+      (* (+ 1 (/ t 100)))
+      (- d))))
 
 (defremote email-domain-errors [email]
   (v/email-domain-errors email))
-
-(def app (-> (var handler)
-             (wrap-rpc)
-             (site)))
