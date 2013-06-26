43d8f863b0f2e818a2d04d57a4ad4b23bbe6ba3c
Giacomo Cosenza <giacomo.cosenza@sinapsi.com>
2013-06-26 17:55:32 +0200

    added shopping template

---
+++ src/clj/modern_cljs/templates/shopping.clj
@@ -0,0 +1,12 @@
+(ns modern-cljs.templates.shopping
+  (:require [net.cgrand.enlive-html :refer [deftemplate set-attr]]
+            [modern-cljs.remotes :refer [calculate]]))
+
+(deftemplate shopping "public/shopping.html"
+  [quantity price tax discount]
+  [:#quantity] (set-attr :value quantity)
+  [:#price] (set-attr :value price)
+  [:#tax] (set-attr :value tax)
+  [:#discount] (set-attr :value discount)
+  [:#total] (set-attr :value 
+                      (format "%.2f" (calculate quantity price tax discount))))
