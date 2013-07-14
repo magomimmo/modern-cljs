220ce4fe44e438f3035ceb614161549c5ecc7514
magomimmo <mimmo.cosenza@gmail.com>
2013-07-14 15:34:47 +0200

    small corrections

--- src/clj/modern_cljs/templates/shopping.clj
+++ src/clj/modern_cljs/templates/shopping.clj
@@ -1,5 +1,5 @@
 (ns modern-cljs.templates.shopping
-  (:require [net.cgrand.enlive-html :refer [deftemplate content substitute do-> add-class set-attr attr=]]
+  (:require [net.cgrand.enlive-html :refer [deftemplate content do-> add-class set-attr attr=]]
             [modern-cljs.remotes :refer [calculate]]
             [modern-cljs.shopping.validators :refer [validate-shopping-form]]))
 
@@ -11,18 +11,23 @@
 
 (deftemplate update-shopping-form "public/shopping.html"
   [q p t d errors]
-  ;; select and transform input label
+
+  ;; select and transform input labels
+
   [[:label (attr= :for "quantity")]] (maybe-error (first (:quantity errors)))
   [[:label (attr= :for "price")]] (maybe-error (first (:price errors)))
   [[:label (attr= :for "tax")]] (maybe-error (first (:tax errors)))
   [[:label (attr= :for "discount")]] (maybe-error (first (:discount errors)))
-  ;; select and transform input value
+
+  ;; select and transform input values
+
   [:#quantity] (set-attr :value q)
   [:#price] (set-attr :value p)
   [:#tax] (set-attr :value t)
   [:#discount] (set-attr :value d)
 
-  ;; select and teransform total
+  ;; select and transform total
+
   [:#total] (if errors
               (set-attr :value "0.00")
               (set-attr :value (format "%.2f" (calculate q p t d)))))
