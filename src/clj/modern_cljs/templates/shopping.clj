6b39e64a2c6c0adac1767a05588460bf4ba212d4
magomimmo <mimmo.cosenza@gmail.com>
2013-07-14 16:08:41 +0200

    added enlive transformations

--- src/clj/modern_cljs/templates/shopping.clj
+++ src/clj/modern_cljs/templates/shopping.clj
@@ -1,12 +1,36 @@
 (ns modern-cljs.templates.shopping
-  (:require [net.cgrand.enlive-html :refer [deftemplate set-attr]]
-            [modern-cljs.remotes :refer [calculate]]))
+  (:require [net.cgrand.enlive-html :refer [deftemplate content do-> add-class set-attr attr=]]
+            [modern-cljs.remotes :refer [calculate]]
+            [modern-cljs.shopping.validators :refer [validate-shopping-form]]))
 
-(deftemplate shopping "public/shopping.html"
-  [quantity price tax discount]
-  [:#quantity] (set-attr :value quantity)
-  [:#price] (set-attr :value price)
-  [:#tax] (set-attr :value tax)
-  [:#discount] (set-attr :value discount)
-  [:#total] (set-attr :value 
-                      (format "%.2f" (calculate quantity price tax discount))))
+(defmacro maybe-error [expr] 
+  `(if-let [x# ~expr] 
+     (do-> (add-class "error")
+           (content x#))
+     identity))
+
+(deftemplate update-shopping-form "public/shopping.html"
+  [q p t d errors]
+
+  ;; select and transform input labels
+
+  [[:label (attr= :for "quantity")]] (maybe-error (first (:quantity errors)))
+  [[:label (attr= :for "price")]] (maybe-error (first (:price errors)))
+  [[:label (attr= :for "tax")]] (maybe-error (first (:tax errors)))
+  [[:label (attr= :for "discount")]] (maybe-error (first (:discount errors)))
+
+  ;; select and transform input values
+
+  [:#quantity] (set-attr :value q)
+  [:#price] (set-attr :value p)
+  [:#tax] (set-attr :value t)
+  [:#discount] (set-attr :value d)
+
+  ;; select and transform total
+
+  [:#total] (if errors
+              (set-attr :value "0.00")
+              (set-attr :value (format "%.2f" (calculate q p t d)))))
+
+(defn shopping [q p t d]
+  (update-shopping-form q p t d (validate-shopping-form q p t d)))
