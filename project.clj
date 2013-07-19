7860a76d54b4afae4785ea46fb8d986d64eb91ed
Giacomo Cosenza <giacomo.cosenza@sinapsi.com>
2013-07-19 10:33:21 +0200

    pretty-print false

--- project.clj
+++ project.clj
@@ -69,7 +69,10 @@
                            :output-to "resources/public/js/modern_pre.js"
 
                            ;; simple optimization
-                           :optimizations :simple}}
+                           :optimizations :simple
+
+                           ;; no need prettification
+                           :pretty-print false}}
                :prod
                {;; same path as above
                 :source-paths ["src/cljs" "target/test/cljs"]
@@ -78,4 +81,7 @@
                            :output-to "resources/public/js/modern.js"
 
                            ;; advanced optimization
-                           :optimizations :advanced}}}})
+                           :optimizations :advanced
+
+                           ;; no need prettification
+                           :pretty-print false}}}})
