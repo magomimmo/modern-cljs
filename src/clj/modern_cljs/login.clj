06dcc534a3409fab8c1fecb2fd8e4fccf114ff5e
Magomimmo <mimmo.cosenza@gmail.com>
2013-07-26 10:58:20 +0200

    working

--- src/clj/modern_cljs/login.clj
+++ src/clj/modern_cljs/login.clj
@@ -3,7 +3,8 @@
             [modern-cljs.login.java.validators :refer [email-domain-errors]]))
 
 (defn authenticate-user [email password]
-  (if (or (boolean (user-credential-errors email password)) (boolean (email-domain-errors email)))
+  (if (or (boolean (user-credential-errors email password)) 
+          (boolean (email-domain-errors email)))
     (str "Please complete the form.")
     (str email " and " password
            " passed the formal validation, but we still have to authenticate you")))
