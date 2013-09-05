f6dcc8cc93c948a0dcca1fe5e3c8739bcb4246bb
Magomimmo <mimmo.cosenza@gmail.com>
2013-09-05 15:19:15 +0200

    upgraded lein-ring to 0.8.7

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
                  [hiccups "0.2.0"]]
 
   :plugins [[lein-cljsbuild "0.3.2"]
-            [lein-ring "0.8.5"]]
+            [lein-ring "0.8.7"]]
 
   ;; enable cljsbuild tasks support
   ;;:hooks [leiningen.cljsbuild]
