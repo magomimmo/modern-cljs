9add03e566c4f5e8c4d190fb70f96272ec2dc2d0
magomimmo <mimmo.cosenza@gmail.com>
2013-09-12 20:08:33 +0200

    added CLJS builds for unit testing

--- profiles.clj
+++ profiles.clj
@@ -1,9 +1,6 @@
-{:dev {:source-paths ["src/clj"]
-       :test-paths ["target/test/clj"]
+{:dev {:hooks [leiningen.cljsbuild]
 
-       :dependencies [[org.clojure/clojure "1.5.1"]
-                      [org.clojure/clojurescript "0.0-1847"]
-                      [com.cemerick/clojurescript.test "0.0.4"]
+       :dependencies [[com.cemerick/clojurescript.test "0.0.4"]
                       [com.cemerick/piggieback "0.1.0"]]
        
        :plugins [[lein-cljsbuild "0.3.2"]
@@ -21,37 +18,90 @@
                                 valip.predicates
                                 modern-cljs.login.validators
                                 modern-cljs.shopping.validators]
+                   ;; for unit testing with phantomjs
+                   :test-commands {"phantomjs-whitespace"
+                                   ["runners/phantomjs.js" "target/test/js/testable_dbg.js"]
 
-                   :test-commands {"whitespace"
-                                   ["runners/phantomjs.js" 
-                                    "resources/public/js/modern_dbg.js"]
-                                   
-                                   "simple"
-                                   ["runners/phantomjs.js" 
-                                    "resources/public/js/modern_pre.js"]
-                                   
-                                   "advanced"
-                                   ["runners/phantomjs.js" 
-                                    "resources/public/js/modern.js"]}
-                   
+                                   "phantomjs-simple"
+                                   ["runners/phantomjs.js" "target/test/js/testable_pre.js"]
+
+                                   "phantomjs-advanced"
+                                   ["runners/phantomjs.js" "target/test/js/testable.js"]}
                    :builds
+                   {:ws-unit-tests
+                    { ;; clojurescript source code path
+                     :source-paths ["src/brepl" "src/cljs" "target/test/cljs"]
 
-                   {:dev
-                    {:source-paths ["src/brepl" "src/cljs" "target/test/cljs"]
-                     :compiler {:output-to "resources/public/js/modern_dbg.js"
+                     ;; Google Closure Compiler options
+                     :compiler { ;; the name of emitted JS script file
+                                :output-to "target/test/js/testable_dbg.js"
+
+                                ;; minimum optimization
                                 :optimizations :whitespace
+                                ;; prettyfying emitted JS
                                 :pretty-print true}}
+               
+                    :simple-unit-tests
+                    { ;; same path as above
+                     :source-paths ["src/brepl" "src/cljs" "target/test/cljs"]
 
-                    :pre-prod
-                    {:source-paths ["src/brepl" "src/cljs" "target/test/cljs"]
-                     :compiler {:output-to "resources/public/js/modern_pre.js"
+                     :compiler { ;; different JS output name
+                                :output-to "target/test/js/testable_pre.js"
+
+                                ;; simple optimization
                                 :optimizations :simple
+
+                                ;; no need prettification
                                 :pretty-print false}}
-                    
-                    :prod
-                    {:source-paths ["src/cljs" "target/test/cljs"]
-                     :compiler {:output-to "resources/public/js/modern.js"
+               
+                    :advanced-unit-tests
+                    { ;; same path as above
+                     :source-paths ["src/cljs" "target/test/cljs"]
+
+                     :compiler { ;; different JS output name
+                                :output-to "target/test/js/testable.js"
+
+                                ;; advanced optimization
                                 :optimizations :advanced
+
+                                ;; no need prettification
+                                :pretty-print false}}
+               
+                    :dev
+                    { ;; clojurescript source code path
+                     :source-paths ["src/brepl" "src/cljs"]
+
+                     ;; Google Closure Compiler options
+                     :compiler { ;; the name of emitted JS script file
+                                :output-to "resources/public/js/modern_dbg.js"
+
+                                ;; minimum optimization
+                                :optimizations :whitespace
+                                ;; prettyfying emitted JS
+                                :pretty-print true}}
+                    :pre-prod
+                    { ;; same path as above
+                     :source-paths ["src/brepl" "src/cljs"]
+
+                     :compiler { ;; different JS output name
+                                :output-to "resources/public/js/modern_pre.js"
+
+                                ;; simple optimization
+                                :optimizations :simple
+
+                                ;; no need prettification
+                                :pretty-print false}}
+                    :prod
+                    { ;; same path as above
+                     :source-paths ["src/cljs"]
+
+                     :compiler { ;; different JS output name
+                                :output-to "resources/public/js/modern.js"
+
+                                ;; advanced optimization
+                                :optimizations :advanced
+
+                                ;; no need prettification
                                 :pretty-print false}}}}
        
        :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
