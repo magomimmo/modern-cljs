a254cf3b2614f996538d441e6b2bf4d319bfc0e7
magomimmo <mimmo.cosenza@gmail.com>
2013-09-12 20:06:55 +0200

    added CLJS builds for unit testing

--- project.clj
+++ project.clj
@@ -5,13 +5,19 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.2.0"
   
-  :dependencies [[compojure "1.1.5"]
+  :source-paths ["src/clj"]
+  :test-paths ["target/test/clj"]
+  
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1847"]
+                 [compojure "1.1.5"]
                  [hiccups "0.2.0"]
                  [domina "1.0.2-SNAPSHOT"]
                  [shoreleave/shoreleave-remote-ring "0.3.0"]
                  [shoreleave/shoreleave-remote "0.3.0"]
                  [com.cemerick/valip "0.3.2"]
                  [enlive "1.1.4"]]
-
+  
   :plugins [[lein-ring "0.8.7"]]
   :ring {:handler modern-cljs.core/app})
+
