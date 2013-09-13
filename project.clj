097664eb4a87f01d4b4477ed863a005228bd553c
magomimmo <mimmo.cosenza@gmail.com>
2013-09-14 00:28:41 +0200

    cljsbuild updated to 0.3.3

--- project.clj
+++ project.clj
@@ -11,6 +11,7 @@
   :test-paths ["test/clj"]
 
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1847"]
                  [compojure "1.1.5"]
                  [hiccups "0.2.0"]
                  [domina "1.0.2-SNAPSHOT"]
@@ -19,7 +20,7 @@
                  [com.cemerick/valip "0.3.2"]
                  [enlive "1.1.4"]]
 
-  :plugins [[lein-cljsbuild "0.3.2"]
+  :plugins [[lein-cljsbuild "0.3.3"]
             [lein-ring "0.8.7"]]
 
   :hooks [leiningen.cljsbuild]
