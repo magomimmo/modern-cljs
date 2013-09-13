9c7659408c786a99145e2d26956981ddd2b70906
magomimmo <mimmo.cosenza@gmail.com>
2013-09-14 00:10:03 +0200

    cljsbuild updated to 0.3.3

--- project.clj
+++ project.clj
@@ -9,11 +9,12 @@
   :source-paths ["src/clj"]
 
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1847"]
                  [compojure "1.1.5"]
                  [domina "1.0.2-SNAPSHOT"]
                  [hiccups "0.2.0"]]
 
-  :plugins [[lein-cljsbuild "0.3.2"]
+  :plugins [[lein-cljsbuild "0.3.3"]
             [lein-ring "0.8.7"]]
 
   ;; enable cljsbuild tasks support
