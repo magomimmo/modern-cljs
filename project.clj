207217f570d0f324af47421e0ad16555e00f01b3
magomimmo <mimmo.cosenza@gmail.com>
2013-09-28 02:32:29 +0200

    updated to cljs 0.0-1878

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
   :source-paths ["src/clj"]
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1847"]
+                 [org.clojure/clojurescript "0.0-1878"]
                  [compojure "1.1.5"]
                  [domina "1.0.2-SNAPSHOT"]
                  [hiccups "0.2.0"]]
