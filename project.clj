273258e48ec811fdc38bc2f0a051a743c8f94d87
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2013-10-22 15:24:57 +0200

    downgraded cljs to 1847

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/magomimmo/modern-cljs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
- 
-  :pom-addition [:developers [:developer 
+
+  :pom-addition [:developers [:developer
                               [:id "magomimmo"]
                               [:name "Mimmo Cosenza"]
                               [:url "https://github.com/magomimmo"]
@@ -17,9 +17,9 @@
   :source-paths ["src/clj"]
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1913"]]
+                 [org.clojure/clojurescript "0.0-1847"]]
 
-  :plugins [[lein-cljsbuild "0.3.3"]]
+  :plugins [[lein-cljsbuild "0.3.4"]]
 
   ;; cljsbuild tasks configuration
   :cljsbuild {:builds
