54e622693c6de4fad26c3594549818e2e95a4a61
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2013-10-30 17:47:57 +0100

    added src/cljs to main source-paths option

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
   :min-lein-version "2.0.0"
 
   ;; clojure source code path
-  :source-paths ["src/clj"]
+  :source-paths ["src/clj" "src/cljs"]
 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1978"]
