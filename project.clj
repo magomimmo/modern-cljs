2fb4cb8e6e50ff599a364ebaa80bc059dd83cdd5
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2013-11-20 12:28:39 +0100

    update cljs to 0.0-2030, compojure to 1.1.6, shoreleave to 0.3.1-SNAPSHOT, lein-cljsbuild to 1.0.0 and lein-ring to 0.8.8

--- project.clj
+++ project.clj
@@ -19,17 +19,17 @@
   :source-paths ["src/clj" "src/cljs" "src/brepl"]
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1847"]
-                 [compojure "1.1.5"]
+                 [org.clojure/clojurescript "0.0-2030"]
+                 [compojure "1.1.6"]
                  [hiccups "0.2.0"]
                  [domina "1.0.3-SNAPSHOT"]
-                 [shoreleave/shoreleave-remote-ring "0.3.0"]
-                 [shoreleave/shoreleave-remote "0.3.0"]
+                 [org.clojars.magomimmo/shoreleave-remote-ring "0.3.1-SNAPSHOT"]
+                 [org.clojars.magomimmo/shoreleave-remote "0.3.1-SNAPSHOT"]
                  [com.cemerick/valip "0.3.2"]
                  [enlive "1.1.4"]]
 
-  :plugins [[lein-cljsbuild "0.3.4"]
-            [lein-ring "0.8.7"]]
+  :plugins [[lein-cljsbuild "1.0.0"]
+            [lein-ring "0.8.8"]]
 
   ;; ring tasks configuration
   :ring {:handler modern-cljs.core/app}
