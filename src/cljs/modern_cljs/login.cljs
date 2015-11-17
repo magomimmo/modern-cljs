cae95ed2040a12d4d352aff9e16a83f32a213ec2
Mimmo Cosenza <mimmo.cosenza@gmail.com>
2015-11-17 01:50:17 +0100

    use domina

--- src/cljs/modern_cljs/login.cljs
+++ src/cljs/modern_cljs/login.cljs
@@ -1,18 +1,13 @@
-(ns modern-cljs.login)
+(ns modern-cljs.login
+  (:require [domina :refer [by-id value]]))
 
-;; define the function to be attached to form submission event
 (defn validate-form []
-  ;; get email and password element from their ids in the HTML form
-  (let [email (.getElementById js/document "email")
-        password (.getElementById js/document "password")]
-    (if (and (> (count (.-value email)) 0)
-             (> (count (.-value password)) 0))
-      true
-      (do (js/alert "Please, complete the form!")
-          false))))
+  (if (and (> (count (value (by-id "email"))) 0)
+           (> (count (value (by-id "password"))) 0))
+    true
+    (do (js/alert "Please, complete the form!")
+        false)))
 
-;; define the function to attach validate-form to onsubmit event of
-;; the form
 (defn init []
   ;; verify that js/document exists and that it has a getElementById
   ;; property
