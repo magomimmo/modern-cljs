f3abe01d446fc745bd6a5cfcd64241bab4da5194
Mimmo Cosenza <mimmo.cosenza@gmail.com>
2015-11-18 09:45:28 +0100

    delete core.cljs

--- src/cljs/modern_cljs/core.cljs
+++
@@ -1,8 +0,0 @@
-;; create the main project namespace
-(ns modern-cljs.core)
-
-;; enable cljs to print to the JS console of the browser
-(enable-console-print!)
-
-;; print to the console
-(println "Hello, World!")
