33805836f96d430dc00f151243f3dc6369eef46e
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2015-11-18 20:20:57 +0100

    done

--- src/cljs/modern_cljs/shopping.cljs
+++ src/cljs/modern_cljs/shopping.cljs
@@ -1,5 +1,6 @@
 (ns modern-cljs.shopping
-  (:require [domina :refer [by-id value set-value!]]))
+  (:require [domina :refer [by-id value set-value!]]
+            [domina.events :refer [listen!]]))
 
 (defn calculate []
   (let [quantity (value (by-id "quantity"))
@@ -9,11 +10,9 @@
     (set-value! (by-id "total") (-> (* quantity price)
                                     (* (+ 1 (/ tax 100)))
                                     (- discount)
-                                    (.toFixed 2)))
-    false))
+                                    (.toFixed 2)))))
 
 (defn ^:export init []
   (if (and js/document
            (.-getElementById js/document))
-    (let [the-form (.getElementById js/document "shoppingForm")]
-      (set! (.-onsubmit the-form) calculate))))
+    (listen! (by-id "calc") :click calculate)))
