f24b641521de2650531ae7f4bd15309567d06725
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2015-11-25 19:52:05 +0100

    working on it

--- src/cljs/modern_cljs/login.cljs
+++ src/cljs/modern_cljs/login.cljs
@@ -2,6 +2,13 @@
   (:require [domina.core :refer [by-id value]]
             [domina.events :refer [listen! prevent-default]]))
 
+;;; 4 to 8, at least one numeric digit.
+(def ^:dynamic *password-re* 
+  #"^(?=.*\d).{4,8}$")
+
+(def ^:dynamic *email-re* 
+  #"^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$")
+
 (defn validate-form [e]
   (if (or (empty? (value (by-id "email")))
           (empty? (value (by-id "password"))))
