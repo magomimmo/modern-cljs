3fe5279214979c8d591a7f3ed4235d22253e6a18
Mimmo Cosenza <mimmo.cosenza@gmail.com>
2015-11-27 08:48:36 +0100

    add pattern attr

--- src/cljs/modern_cljs/login.cljs
+++ src/cljs/modern_cljs/login.cljs
@@ -4,21 +4,15 @@
                                  by-id
                                  destroy!
                                  prepend!
-                                 value]]
+                                 value
+                                 attr]]
             [domina.events :refer [listen! prevent-default]]
             [hiccups.runtime])
   (:require-macros [hiccups.core :refer [html]]))
 
-;;; 4 to 8, at least one numeric digit.
-(def ^:dynamic *password-re* 
-  #"^(?=.*\d).{4,8}$")
-
-(def ^:dynamic *email-re* 
-  #"^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$")
-
 (defn validate-email [email]
   (destroy! (by-class "email"))
-  (if (not (re-matches *email-re* (value email)))
+  (if (not (re-matches (re-pattern (attr email :pattern)) (value email)))
     (do
       (prepend! (by-id "loginForm") (html [:div.help.email "Wrong email"]))
       false)
@@ -26,7 +20,7 @@
 
 (defn validate-password [password]
   (destroy! (by-class "password"))
-  (if (not (re-matches *password-re* (value password)))
+  (if (not (re-matches (re-pattern (attr password :pattern)) (value password)))
     (do
       (append! (by-id "loginForm") (html [:div.help.password "Wrong password"]))
       false)
