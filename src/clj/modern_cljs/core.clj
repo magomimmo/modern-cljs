5409035f08bf88d47cc7e4eb13c9af5b2b4b4981
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2015-11-27 16:48:58 +0100

    add /login POST and authenticate-user

--- src/clj/modern_cljs/core.clj
+++ src/clj/modern_cljs/core.clj
@@ -1,9 +1,11 @@
 (ns modern-cljs.core 
-  (:require [compojure.core :refer [defroutes GET]]
-            [compojure.route :refer [not-found files resources]]))
+  (:require [compojure.core :refer [defroutes GET POST]]
+            [compojure.route :refer [not-found files resources]]
+            [modern-cljs.login :refer [authenticate-user]]))
 
 (defroutes handler
   (GET "/" [] "Hello from Compojure!")  ;; for testing only
   (files "/" {:root "target"})          ;; to serve static resources
+  (POST "/login" [email password] (authenticate-user email password))
   (resources "/" {:root "target"})      ;; to serve anything else
   (not-found "Page Not Found"))         ;; page not found
