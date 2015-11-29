f606ad1f43f48c9c5f7f863998407ba070ca1769
Mimmo Cosenza <mimmo.cosenza@gmail.com>
2015-11-29 22:47:34 +0100

    refactor login

--- src/cljs/modern_cljs/login.cljs
+++ src/cljs/modern_cljs/login.cljs
@@ -7,47 +7,41 @@
                                  value
                                  attr]]
             [domina.events :refer [listen! prevent-default]]
-            [hiccups.runtime])
+            [hiccups.runtime]
+            [modern-cljs.login.validators :refer [user-credential-errors]])
   (:require-macros [hiccups.core :refer [html]]))
 
 (defn validate-email [email]
   (destroy! (by-class "email"))
-  (if (not (re-matches (re-pattern (attr email :pattern)) (value email)))
+  (if-let [errors (:email (user-credential-errors (value email) nil))]
     (do
-      (prepend! (by-id "loginForm") (html [:div.help.email (attr email :title)]))
+      (prepend! (by-id "loginForm") (html [:div.help.email (first errors)]))
       false)
     true))
 
 (defn validate-password [password]
   (destroy! (by-class "password"))
-  (if (not (re-matches (re-pattern (attr password :pattern)) (value password)))
+  (if-let [errors (:password (user-credential-errors nil (value password)))]
     (do
-      (append! (by-id "loginForm") (html [:div.help.password (attr password :title)]))
+      (append! (by-id "loginForm") (html [:div.help.password (first errors)]))
       false)
     true))
 
-(defn validate-form [evt]
-  (let [email (by-id "email")
-        password (by-id "password")
-        email-val (value email)
-        password-val (value password)]
-    (if (or (empty? email-val) (empty? password-val))
+(defn validate-form [evt email password]
+  (if-let [{e-errs :email p-errs :password} (user-credential-errors (value email) (value password))]
+    (if (or e-errs p-errs)
       (do
         (destroy! (by-class "help"))
         (prevent-default evt)
-        (append! (by-id "loginForm") 
-                 (html [:div.help "Please complete the form"])))
-      (if (and (validate-email email)
-               (validate-password password))
-        true
-        (prevent-default evt)))))
+        (append! (by-id "loginForm") (html [:div.help "Please complete the form."])))
+      (prevent-default evt))
+    true))
 
 (defn ^:export init []
   (if (and js/document
            (aget js/document "getElementById"))
     (let [email (by-id "email")
           password (by-id "password")]
-      (listen! (by-id "submit") :click (fn [evt] (validate-form evt)))
+      (listen! (by-id "submit") :click (fn [evt] (validate-form evt email password)))
       (listen! email :blur (fn [evt] (validate-email email)))
       (listen! password :blur (fn [evt] (validate-password password))))))
-
