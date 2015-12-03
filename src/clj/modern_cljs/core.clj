2869cd037c1327af08d54564cc98eda9b784b207
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2015-12-03 16:06:58 +0100

    move app handler here

--- src/clj/modern_cljs/core.clj
+++ src/clj/modern_cljs/core.clj
@@ -1,6 +1,8 @@
 (ns modern-cljs.core 
   (:require [compojure.core :refer [defroutes GET POST]]
             [compojure.route :refer [not-found files resources]]
+            [compojure.handler :refer [site]]
+            [shoreleave.middleware.rpc :refer [wrap-rpc]]
             [modern-cljs.login :refer [authenticate-user]]
             [modern-cljs.templates.shopping :refer [shopping]]))
 
@@ -12,3 +14,8 @@
         (shopping quantity price tax discount))
   (resources "/" {:root "target"})      ;; to serve anything else
   (not-found "Page Not Found"))         ;; page not found
+
+(def app
+  (-> handler
+      (wrap-rpc)
+      (site)))
