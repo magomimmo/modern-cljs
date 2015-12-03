33f0b1f096c76c6149383e65a9a00494df94757a
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2015-12-03 16:08:57 +0100

    add calculate

--- src/clj/modern_cljs/templates/shopping.clj
+++ src/clj/modern_cljs/templates/shopping.clj
@@ -1,10 +1,17 @@
 (ns modern-cljs.templates.shopping
-  (:require [net.cgrand.enlive-html :refer [deftemplate set-attr]]))
+  (:require [net.cgrand.enlive-html :refer [deftemplate set-attr]]
+            [modern-cljs.remotes :refer [calculate]]))
 
 (deftemplate shopping "shopping.html"
   [quantity price tax discount]
   [:#quantity] (set-attr :value quantity)
   [:#price] (set-attr :value price)
   [:#tax] (set-attr :value tax)
-  [:#discount] (set-attr :value discount))
+  [:#discount] (set-attr :value discount)
+  [:#total] (set-attr :value (format "%.2f" (calculate 
+                                             (read-string quantity)
+                                             (read-string price)
+                                             (read-string tax)
+                                             (read-string discount)))))
+
 
