823a39474b94a7d84493fa17b1790bcd5f495ca8
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2015-12-17 19:23:04 +0100

    delete unused files

--- src/brepl/modern_cljs/connect.cljs
+++
@@ -1,4 +0,0 @@
-(ns modern-cljs.connect
-  (:require [clojure.browser.repl :as repl]))
-
-(repl/connect "http://localhost:9000/repl")
--- src/clj/modern_cljs/login/java/validators.clj
+++
@@ -1,7 +0,0 @@
-(ns modern-cljs.login.java.validators
-  (:require [valip.core :refer [validate]]
-            [valip.java.predicates :refer [valid-email-domain?]]))
-
-(defn email-domain-errors [email]
-  (validate {:email email}
-            [:email valid-email-domain? "The domain of the email doesn't exist."]))
