4fb38c6944f0d04501e04eb2ead8e18edcf3114d
Mimmo Cosenza <mimmo.cosenza@gmail.com>
2015-12-30 01:00:51 +0100

    add bang

--- src/cljs/modern_cljs/shopping.cljs
+++ src/cljs/modern_cljs/shopping.cljs
@@ -12,27 +12,28 @@
             [domina.events :refer [listen! prevent-default]]
             [domina.css :refer [sel]]
             [hiccups.runtime]
-            [modern-cljs.shopping.validators :as v]
+            [modern-cljs.shopping.validators :refer [validate-shopping-field
+                                                     validate-shopping-form]]
             [shoreleave.remotes.http-rpc :refer [remote-callback]])
   (:require-macros [hiccups.core :refer [html]]
                    [shoreleave.remotes.macros :as macros]))
 
-(defn validate-shopping-field [field text]
+(defn validate-shopping-field! [field text]
   (let [attr (name field)
         label (sel (str "label[for=" attr "]"))]
     (remove-class! label "help")
-    (if-let [error (v/validate-shopping-field field (value (by-id attr)))]
+    (if-let [error (validate-shopping-field field (value (by-id attr)))]
       (do
         (add-class! label "help")
         (set-text! label error))
       (set-text! label text))))
 
-(defn calculate [evt]
+(defn calculate! [evt]
   (let [quantity (value (by-id "quantity"))
         price (value (by-id "price"))
         tax (value (by-id "tax"))
         discount (value (by-id "discount"))
-        errors (v/validate-shopping-form quantity price tax discount)]
+        errors (validate-shopping-form quantity price tax discount)]
     (if-not errors
       (remote-callback :calculate
                        [quantity price tax discount]
@@ -50,24 +51,24 @@
       ;; quantity validation
       (listen! (by-id "quantity")
                :blur
-               (fn [_] (validate-shopping-field :quantity quantity-text)))
+               (fn [_] (validate-shopping-field! :quantity quantity-text)))
       ;; price validation
       (listen! (by-id "price")
                :blur
-               (fn [_] (validate-shopping-field :price price-text)))
+               (fn [_] (validate-shopping-field! :price price-text)))
       ;; tax validation
       (listen! (by-id "tax")
                :blur
-               (fn [_] (validate-shopping-field :tax tax-text)))
+               (fn [_] (validate-shopping-field! :tax tax-text)))
       ;; discount validation
       (listen! (by-id "discount")
                :blur
-               (fn [_] (validate-shopping-field :discount discount-text))))
+               (fn [_] (validate-shopping-field! :discount discount-text))))
     
     ;; calculate
     (listen! (by-id "calc") 
              :click 
-             (fn [evt] (calculate evt)))
+             (fn [evt] (calculate! evt)))
     ;; show help
     (listen! (by-id "calc") 
              :mouseover 
